d02c7f9c52b211f697de2d4bfae3df0062601498
rividya <ivy.aluna@gmail.com>
2020-11-16 12:37:40 +0700

    XML

---
+++ app/src/main/java/com/example/healthelper/Article.kt
@@ -0,0 +1,12 @@
+package com.example.healthelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Article : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article)
+    }
+}
---
+++ app/src/main/java/com/example/healthelper/DetailBooking.kt
@@ -0,0 +1,12 @@
+package com.example.healthelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailBooking : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_booking)
+    }
+}
